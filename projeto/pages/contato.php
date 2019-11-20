926eab70d8af7d72a5587c01bc077b52fee944e6
evertonschultz <evertonf.m.schultz98@gmail.com>
2019-11-20 15:34:57 -0300

    Add contact page
    Foi adicionado a página de contato, assim como a estilização .css para ela em 'projeto/assests/css/styles-contato.css'

--- projeto/pages/contato.php
+++ projeto/pages/contato.php
@@ -1,16 +1,126 @@
-		
+<link rel="stylesheet" href="<?php echo INCLUDE_PATH; ?>assets/css/styles-contato.css" />
 
-<!-- main site ( conteúdos do site ) -->
-<main class="main_site">
-	
-	<!-- sessão banner -->
-	<section class="sessao_do_site sessao_">
-		<div class="lista_sessao">
+<!-- container site -->
+<div class="container_site">
+	<!-- main site ( conteúdos do site ) -->
+	<main class="main_site">
+		<!-- sessão contatos -->
+		<section class="sessao_do_site sessao_pad sessao_contatos">
+			<div class="container">
 
-		</div>
-	</section>
-	<!-- / sessão banner -->
+				<div class="lista_sessao">
 
+					<div class="contatos">
+						<ul class="contatos_ul row justify-content-center hzmp">
 
-</main>
-<!-- / main site ( conteúdos do site ) -->
+							<li class="contatos_li col-11 col-sm-6 col-lg-3 col-xl-4">
+								<div class="contatos_content">
+
+									<div class="box_contatos">
+
+										<div class="contatos_telefones">
+											<div class="titulo_contatos">
+												<h3 class="contatos_h3">Contato</h3>
+											</div>
+											<ul class="contatos_telefones_ul hzmp">
+
+												<li class="contatos_telefones_li"><img src="https://www.agenciafaro.com.br/wp-content/themes/agenciafaronovo/assets/novo-site/images/phone.svg" alt="" title="" class="contatos_telefones_img img-fluid" />
+													<h4 class="contatos_telefones_h4 hzmp">(00) 3333 3333</h4>
+												</li>
+
+												<li class="contatos_telefones_li"><img src="https://www.agenciafaro.com.br/wp-content/themes/agenciafaronovo/assets/novo-site/images/whatsapp.svg" alt="" title="" class="contatos_telefones_img img-fluid" />
+													<h4 class="contatos_telefones_h4 hzmp">(00) 9 9999 9999</h4>
+												</li>
+
+											</ul>
+										</div>
+										<div class="contatos_redessociais">
+											<div class="titulo_contatos">
+												<h3 class="contatos_h3">Redes Sociais</h3>
+											</div>
+											<ul class="contatos_redessociais_ul hzmp">
+
+												<li class="contatos_redessociais_li"><a class="contatos_redessociais_a" title="" target="_blank" href="https://pt-br.facebook.com/timebeta"><img src="https://image.freepik.com/icones-gratis/logotipo-facebook-com-cantos-arredondados_318-9850.jpg" alt="" title="" class="contatos_redessociais_img img-fluid" /></a></li>
+
+												<li class="contatos_redessociais_li"><a class="contatos_redessociais_a" title="" target="_blank" href="https://www.instagram.com/timebeta"><img src="https://image.flaticon.com/icons/png/512/87/87390.png" alt="" title="" class="contatos_redessociais_img img-fluid" /></a></li>
+
+												<li class="contatos_redessociais_li"><a class="contatos_redessociais_a" title="" target="_blank" href="https://www.linkedin.com/company/timebeta"><img src="https://image.flaticon.com/icons/svg/61/61109.svg" alt="" title="" class="contatos_redessociais_img img-fluid" /></a></li>
+
+											</ul>
+										</div>
+									</div>
+
+								</div>
+							</li>
+
+							<li class="contatos_li col-11 col-sm-6 col-lg-4 col-xl-4">
+								<div class="contatos_content">
+
+									<div class="box_contatos">
+
+										<div class="contatos_endereco">
+											<div class="titulo_contatos">
+												<h3 class="contatos_h3">Endereço</h3>
+											</div>
+											<h4 class="contatos_endereco_h4">Rua Martinho Lutero, 0000</h4>
+											<h4 class="contatos_endereco_h4">Cachoeira do Sul | RS</h4>
+										</div>
+
+										<div class="contatos_email">
+											<div class="titulo_contatos">
+												<h3 class="contatos_h3">E-mail</h3>
+											</div>
+											<h4 class="contatos_email_h4">contato@timebeta.com.br</h4>
+										</div>
+
+									</div>
+
+								</div>
+							</li>
+
+							<li class="contatos_li col-11 col-sm-12 col-lg-5 col-xl-4">
+								<div class="contatos_content">
+
+									<div role="form" class="wpcf7" id="wpcf7-f3042-o1" lang="pt-BR" dir="ltr">
+										<div class="screen-reader-response"></div>
+										<form action="#" method="post" class="wpcf7-form" novalidate="novalidate">
+											<div style="display: none;">
+												<input type="hidden" name="_wpcf7" value="3042">
+												<input type="hidden" name="_wpcf7_version" value="5.1.5">
+												<input type="hidden" name="_wpcf7_locale" value="pt_BR">
+												<input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f3042-o1">
+												<input type="hidden" name="_wpcf7_container_post" value="0">
+												<input type="hidden" name="g-recaptcha-response" value="">
+											</div>
+											<div class="form_contato">
+												<ul class="form_contato_ul hzmp">
+													<li class="form_contato_li">
+														<span class="wpcf7-form-control-wrap nome"><input type="text" name="nome" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required form_f form_campo br05 form_campo_contato" aria-required="true" aria-invalid="false" placeholder="Nome"></span>
+													</li>
+													<li class="form_contato_li">
+														<span class="wpcf7-form-control-wrap telefone"><input type="tel" name="telefone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel form_f form_campo br05 form_campo_contato" aria-required="true" aria-invalid="false" placeholder="Telefone"></span>
+													</li>
+													<li class="form_contato_li">
+														<span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email form_f form_campo br05 form_campo_contato" aria-required="true" aria-invalid="false" placeholder="E-mail"></span>
+													</li>
+													<li class="form_contato_li">
+														<span class="wpcf7-form-control-wrap mensagem"><textarea name="mensagem" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea form_f form_campo br05 form_texto form_texto_contato" aria-invalid="false" placeholder="Sua mensagem"></textarea></span>
+													</li>
+													<input type="submit" value="Enviar mensagem" class="wpcf7-form-control wpcf7-submit btn_layout btn_ br05 form_botao_contato">
+												</ul>
+											</div>
+											<div class="wpcf7-response-output wpcf7-display-none"></div>
+										</form>
+									</div>
+								</div>
+							</li>
+
+						</ul>
+					</div>
+
+				</div>
+
+			</div>
+		</section>
+		<!-- / sessão contatos -->
+</div
